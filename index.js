d810d100a54a3ac8db320e0ef43960cab458d985
Angela Deen <adeen1221@gmail.com>
2018-07-27 01:47:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 function happyHolidays() {return"Happy holidays!";
 }
-function happyHolidaysTo(firstname){ return `Happy holidays, ${name}!`}
+function happyHolidaysTo(firstname){ return `Happy holidays, ${firstname}!`}
 function happyHolidayTo (holiday, firstname){ return `Happy ${Thanksgiving}, ${Beau}!`}
 function holidayCountdown (holiday, days){ return `It's ${three} days until ${Christmas}!`}
