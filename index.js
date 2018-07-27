cc5dfbdbc89a568c401c363c91f94ee23e9f2a3c
Angela Deen <adeen1221@gmail.com>
2018-07-27 01:40:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,5 @@
 function happyHolidays() {return"Happy holidays!";
 }
 function happyHolidaysTo(firstname){ return `Happy holidays, ${name}!`}
+function happyHolidayTo (holiday, firstname){ return `Happy ${holiday}, ${name}!`}
+function holidayCountdown
